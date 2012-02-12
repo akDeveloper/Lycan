a968f9d02011183443e4cf855c0b91b284bec971
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-02-12 16:42:51 +0200

    add set method for single association, when construct from query

--- Record/Associations/Single.php
+++ Record/Associations/Single.php
@@ -4,8 +4,12 @@
 
 namespace Lycan\Record\Associations;
 
-use Lycan\Support\Inflect;
-
-abstract class Single extends \Lycan\Record\Associations
+abstract class Single extends \Lycan\Record\Associations implements Interfaces\Single
 {
+
+    public function setWith(\Lycan\Record\Model $associate)
+    {
+        $this->result_set = $associate;
+    }
+
 }
