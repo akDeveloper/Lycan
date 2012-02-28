2189ada036fec0b138d0f3b6668f34dfd824859b
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-02-28 20:52:15 +0200

    add Null record object

--- Record/Null.php
+++ Record/Null.php
@@ -4,8 +4,18 @@
 
 namespace Lycan\Record;
 
-class Null 
+class Null extends Model
 {
+    public static $columns=array(null);
 
+    public function __get($name)
+    {
+        return new self();
+    }
+
+    public function __toString()
+    {
+        return '';
+    }
 }
 
