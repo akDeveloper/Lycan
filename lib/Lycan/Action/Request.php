0d2a18c1ff5b3884df8a597b7e4b245dbf3755a1
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-03-11 04:54:51 +0200

    add POST to params

--- lib/Lycan/Action/Request.php
+++ lib/Lycan/Action/Request.php
@@ -107,6 +107,11 @@ class Request
         $this->_url = null === $url ? (isset($_SERVER['REQUEST_URI']) ? urldecode($_SERVER['REQUEST_URI']) : null ) : urldecode($url);
 
         $this->_parse_request_url();
+
+        $this->_parse_post_data();
+
+        $this->params = $this->_query_array = $this->sanitize_array($this->_query_array);
+        \Lycan\Support\Logger::getLogger()->log(print_r($this->params, true));
     }
 
     private function _parse_request_url()
@@ -124,11 +129,9 @@ class Request
             ? $this->_port = $parse_url['port']
             : (isset($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : 80);
 
-        $this->_method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+        $this->_method = strtolower(isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET');
 
         $this->_referer = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
-
-        #Lycan_Util_Logger::log("Request Method: " . $this->_method);
 
         $this->_protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : "HTTP/1.0";
 
@@ -136,28 +139,37 @@ class Request
 
         $path = $parse_url['path'];
 
-        #if (Lycan_Core_Application::$environment == 'dev')
-        #Lycan_Util_Logger::log("Parsing url " . $path);
-
         # strip first and last backslash (/) if exist
-        $this->_path = substr($path, -1, 1) == "/" ?
-            substr($path, 0, -1) : $path;
-        $this->_path = substr($this->_path, 0, 1) == "/" ?
-            substr($this->_path, 1) : $this->_path;
+        $this->_path = substr($path, -1, 1) == "/" 
+            ? substr($path, 0, -1) 
+            : $path;
+        $this->_path = substr($this->_path, 0, 1) == "/" 
+            ? substr($this->_path, 1) 
+            : $this->_path;
 
         if (isset($parse_url['query'])) {
             $this->_query = $parse_url['query'];
             parse_str($parse_url['query'], $this->_query_array);
         }
 
-        $this->_query_array = self::sanitize_array($this->_query_array);
+    }
+
+    private function _parse_post_data()
+    {
+        if (isset($_POST) && !empty($_POST)) {
+            $this->_query_array = array_merge($this->_query_array, $_POST);
+            $this->_method = isset($_POST['_method']) && in_array($_POST['_method'], array('put','delete')) 
+                ?  $_POST['_method']
+                : 'post';
+
+        }
     }
 
     public function sanitize_array(array $array)
     {
         foreach ($array as $k => $v) {
             if (is_array($v))
-                $array[$k] = self::sanitize_array($v);
+                $array[$k] = $this->sanitize_array($v);
             else
                 $array[$k] = htmlspecialchars( get_magic_quotes_gpc() ? stripslashes($v) : $v, ENT_QUOTES, 'UTF-8');
         }
